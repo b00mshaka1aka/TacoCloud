7e0c4c46d01dd5ededbbdf2412a5f961b606c308
b00mshaka1aka <shishkinmihail961@gmail.com>
2023-04-05 15:41:56 +0300

    Chapter one

---
+++ src/test/java/com/b00mshaka1aka/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b00mshaka1aka.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
